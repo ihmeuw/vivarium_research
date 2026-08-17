8e2aa41eff37315a2d53aee3713b8a6d05902156
Zeb Burke-Conte <zmbc@uw.edu>
2026-08-17 15:45:49 -0700

    Remove data versioning

--- docs/source/model_design/vivarium_features/modeling_workflow_automation/workflow.py
+++ docs/source/model_design/vivarium_features/modeling_workflow_automation/workflow.py
@@ -2,7 +2,10 @@ from vivarium.workflow import Workflow, ArtifactKey
 
 from tutorial.constants.metadata import LOCATIONS
 
-workflow = Workflow()
+# 'tutorial' indicates the name of the directory within
+# /mnt/team/simulation_science/pub/models/ that artifacts
+# and simulation results should be nested within (by default).
+workflow = Workflow('tutorial')
 
 # We can use a loop over the locations (or other parameters) to avoid repeating the steps
 # for each location.
@@ -23,11 +26,10 @@ for location in workflow.iterate(location=LOCATIONS):
     ]
 
     workflow.simulation_step(
-        name='PAF sim',
+        name='paf_simulation',
         input=keys_needed_for_paf,
         model_specification='src/tutorial/data/custom_paf/paf_model_spec.yaml',
         branches_file='src/tutorial/data/custom_paf/scenarios.yaml',
-        output=f'src/tutorial/data/custom_paf/results/{location}/',
         # These resource requests should be optional (have defaults) but can also be specified
         memory_gb=2,
         runtime='00:30:00',
@@ -40,7 +42,8 @@ for location in workflow.iterate(location=LOCATIONS):
         # The signature of the function would be:
         # def reformat_results(input: pd.DataFrame, location: str) -> pd.DataFrame:
         'tutorial.data.custom_paf.reformat_results.reformat_results',
-        input=f'src/tutorial/data/custom_paf/results/{location}/paf_observer.parquet',
+        # The run_name is dynamically replaced by the workflow with e.g. 'third_run'
+        input=f'/mnt/team/simulation_science/pub/models/tutorial/results/{{run_name}}/paf_simulation/{location}/paf_observer.parquet',
         # The dataframe returned gets automatically saved into the artifact at this key.
         # There would *not* be a function in loader.py for this key.
         output=ArtifactKey('risk_factor.child_wasting.population_attributable_fraction'),
@@ -73,7 +76,7 @@ for location in workflow.iterate(location=LOCATIONS):
     # A test step will halt the workflow if the tests fail.
     # This one saves notebook output to a `executed` subdir for human inspection.
     workflow.test_notebooks_step(
-        name='interactive sim V&V',
+        name='interactive_sim_vnv',
         # The input is the artifact keys, not the results, since interactive sim V&V
         # can run before the sim has been run.
         input=keys_needed_for_main_sim,
@@ -82,26 +85,25 @@ for location in workflow.iterate(location=LOCATIONS):
     )
 
     workflow.simulation_step(
-        name='main sim',
+        name='main_sim',
         artifact_inputs=keys_needed_for_main_sim,
         model_specification='src/tutorial/model_specifications/model_spec.yaml',
         branches_file='src/tutorial/model_specifications/branches/scenarios.yaml',
-        output=f'src/tutorial/results/{location}/',
         memory_gb=2,
         runtime='10:00:00',
     )
 
     # Another test step, this time using the simulation results.
     workflow.test_notebooks_step(
-        name='results V&V',
-        input=f'src/tutorial/results/{location}/',
+        name='results_vnv',
+        input=f'/mnt/team/simulation_science/pub/models/tutorial/results/{{run_name}}/main_sim/{location}/',
         notebooks='tests/results/*.ipynb',
         environment='artifact',
     )
 
     workflow.notebook_step(
         'src/tutorial/results_processing/results_processing.ipynb',
-        input=f'src/tutorial/results/{location}/',
+        input=f'/mnt/team/simulation_science/pub/models/tutorial/results/{{run_name}}/main_sim/{location}/',
     )
 
 
